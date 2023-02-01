9b43eda8b4b9d633c8189fdfa723d1191218a46b
coccigabry <coccigab@gmail.com>
2023-02-01 16:15:36 +0100

    styled home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import posts from '../data.js'
+
 
 const Home = () => {
+
+  const renderPosts = posts.map(post => {
+    const { id, title, desc, img } = post
+    return (
+      <div className='post' key={id}>
+        <div className='img'>
+          <img src={img} alt='' />
+        </div>
+        <div className='content'>
+          <Link className='link' to={`/post/${id}`}>
+            <h1>{title}</h1>
+          </Link>
+            <p>{desc}</p>
+            <button>Read more</button>
+        </div>
+      </div>
+    )
+  })
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='posts'>
+        {renderPosts}
+      </div>
+    </div>
   )
 }
 
